d1232dade6312c8a8dd637f3cea4d256dfad40fc
zenghz22 <zenghz22@mails.tsinghua.edu.cn>
2024-07-20 20:17:21 +0800

    完成了cpu接口，以sram架构面向fakecake

--- mycpu_top.v
+++ mycpu_top.v
@@ -3,18 +3,48 @@
 module mycpu_top(
     input  wire        clk,
     input  wire        resetn,
-    // inst sram interface
-    output wire        inst_sram_en,
-    output wire  [3:0] inst_sram_we,
-    output wire [31:0] inst_sram_addr,
-    output wire [31:0] inst_sram_wdata,
-    input  wire [31:0] inst_sram_rdata,
-    // data sram interface
-    output wire        data_sram_en,
-    output wire  [3:0] data_sram_we,
-    output wire [31:0] data_sram_addr,
-    output wire [31:0] data_sram_wdata,
-    input  wire [31:0] data_sram_rdata,
+    // read req channel
+    output [ 3:0] arid   ,  
+    output [31:0] araddr ,  
+    output [ 7:0] arlen  ,  
+    output [ 2:0] arsize ,  
+    output [ 1:0] arburst, 
+    output [ 1:0] arlock ,  
+    output [ 3:0] arcache,  
+    output [ 2:0] arprot ,  
+    output        arvalid,  
+    input         arready, 
+    // read response channel
+    input [ 3:0]  rid    ,  
+    input [31:0]  rdata  ,  
+    input [ 1:0]  rresp  ,                 
+    input         rlast  ,             
+    input         rvalid , 
+    output        rready ,  
+    // write req channel
+    output [ 3:0] awid   , 
+    output [31:0] awaddr ,  
+    output [ 7:0] awlen  ,  
+    output [ 2:0] awsize ,  
+    output [ 1:0] awburst,  
+    output [ 1:0] awlock ,  
+    output [ 3:0] awcache,  
+    output [ 2:0] awprot ,  
+    output        awvalid,  
+    input         awready,     
+    // write data channel
+    output [ 3:0] wid    ,  
+    output [31:0] wdata  ,  
+    output [ 3:0] wstrb  ,  
+    output        wlast  ,  
+    output        wvalid , 
+    input         wready ,     
+    // write response channel
+    input  [ 3:0] bid    ,      
+    input  [ 1:0] bresp  ,        
+    input         bvalid , 
+    output        bready ,  
+    
     // trace debug interface
     output wire [31:0] debug_wb_pc,
     output wire [ 3:0] debug_wb_rf_we,
@@ -101,5 +131,74 @@ fakecache U_fakecache_data(
             .sram_wdata(data_sram_wdata),
             .sram_rdata(data_sram_rdata));
 
+axi_bridge U_axi_bridge (
+            .clk(clk),
+            .reset(reset),
 
+            .arid(arid),
+            .araddr(araddr),
+            .arlen(arlen),
+            .arsize(arsize),
+            .arburst(arburst),
+            .arlock(arlock),
+            .arcache(arcache),
+            .arprot(arprot),
+            .arvalid(arvalid),
+            .arready(arready),
+
+            .rid(rid),
+            .rdata(rdata),
+            .rresp(rresp),
+            .rlast(rlast),
+            .rvalid(rvalid),
+            .rready(rready),
+
+            .awid(awid),
+            .awaddr(awaddr),
+            .awlen(awlen),
+            .awsize(awsize),
+            .awburst(awburst),
+            .awlock(awlock),
+            .awcache(awcache),
+            .awprot(awprot),
+            .awvalid(awvalid),
+            .awready(awready),
+
+            .wid(wid),
+            .wdata(wdata),
+            .wstrb(wstrb),
+            .wlast(wlast),
+            .wvalid(wvalid),
+            .wready(wready),
+
+            .bid(bid),
+            .bresp(bresp),
+            .bvalid(bvalid),
+            .bready(bready),
+
+            .inst_sram_req      (1'b1      ),
+            .inst_sram_wr       (inst_cache_we       ),
+            .inst_sram_size     (inst_cache_access_sz     ),
+            .inst_sram_addr     (inst_cache_raddr----inst_cache_waddr     ),
+            .inst_sram_wstrb    (1'b1    ),
+            .inst_sram_wdata    (inst_sram_wdata    ),
+            .inst_sram_addr_ok  (  ),
+            .inst_sram_data_ok  (  ),
+            .inst_sram_rdata    (inst_cache_rdata    ),
+
+            .data_sram_req      (1'b1      ),
+            .data_sram_wr       (data_cache_we       ),
+            .data_sram_size     (data_cache_access_sz     ),
+            .data_sram_addr     (data_cache_raddr---data_cache_waddr     ),
+            .data_sram_wstrb    (1'b1    ),
+            .data_sram_wdata    (data_cache_wdata    ),
+            .data_sram_addr_ok  (  ),
+            .data_sram_data_ok  (  ),
+            .data_sram_rdata    (data_cache_rdata    )
+);
 endmodule
+
+
+
+
+
